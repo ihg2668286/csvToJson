64ed562f78134aeab0d7d4e12b7edc9414a7b7bb
croa <croa@blocko.io>
2024-05-20 16:46:38 +0900

    feat: 키 호출부분 수정작업

--- index.js
+++ index.js
@@ -12,30 +12,35 @@ const results = [];
 fs.createReadStream(csvFilePath)
   .pipe(csvParser())
   .on('data', (row) => {
-    // 새 객체를 만들고 필요한 속성만 할당
+    const phoneNumberKey = Object.keys(row).find((key) =>
+      key.includes('업무 전화번호')
+    );
+    const companyLogoKey = Object.keys(row).find((key) =>
+      key.includes('회사 로고 이미지')
+    );
+    const profileImageKey = Object.keys(row).find((key) =>
+      key.includes('프로필 이미지')
+    );
+    const companyAddrKey = Object.keys(row).find((key) =>
+      key.includes('회사 주소지')
+    );
+    const emailKey = Object.keys(row).find((key) =>
+      key.includes('이메일 주소')
+    );
 
+    // 새 객체를 만들고 필요한 속성만 할당
     const obj = {
       userId: row['Submission ID'],
-      email:
-        row[
-          '이메일 주소 오기재시 서비스 이용에 문제가 발생할 수 있으니 이 점 유의하시어 입력해 주시길 바랍니다. '
-        ],
-      name: row['성함'],
-      profileImage: row['프로필 이미지  (미등록 시 노출되지 않습니다.)'],
-      message: row['한 줄 소개글'],
-      phoneNumber:
-        row[
-          '업무 전화번호 (010-0000-0000,02-0000-0000등 하이픈 형식으로 작성 부탁드립니다.)'
-        ],
-      companyEmail: row['회사 이메일'],
-      companyAddr:
-        row['회사 주소지 (입력 권장, 미입력시 지도 보기 기능이 빠집니다.)'],
-      position: row['직함 및 직위'],
       companyName: row['회사명'],
-      companyLogo:
-        row[
-          '회사 로고 이미지 (.svg, .psd 파일로 등록 부탁드리며, 미등록 시 명함에 노출되지 않습니다. 또한 신청 여부와 관계없이 배지를 발급 받을 수 없습니다.)'
-        ],
+      companyLogo: row[companyLogoKey],
+      name: row['성함'],
+      position: row['직함 및 직위'],
+      phoneNumber: row[phoneNumberKey],
+      email: row[emailKey],
+      companyAddr: row[companyAddrKey],
+      profileImage: row[profileImageKey],
+      message: row['한 줄 소개글'],
+      companyEmail: row['회사 이메일'],
     };
 
     // 링크를 추출하고 'links' 배열에 저장
@@ -45,6 +50,7 @@ fs.createReadStream(csvFilePath)
           key.includes(
             '디지털 명함에 넣을 개인 링크를 입력해 주세요. (최대 5개 선택)'
           ) ||
+          key.includes('링크를 입력해 주세요') ||
           key.startsWith('https://1') ||
           key.startsWith('https://2') ||
           key.startsWith('https://3') ||
